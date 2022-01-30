bd98f11507ca2513d299d001c9dad13b71d048b0
clintono1 <clinton1oka@gmail.com>
2022-01-29 22:07:17 -0500

    Update roadmap

--- pages/roadmaps.js
+++ pages/roadmaps.js
@@ -1,71 +1,73 @@
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading} from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { WorkGridItem } from '../components/grid-item'
-import redpug from '../public/images/pug-red.png'
-import greenpug from '../public/images/pug-green.png'
+import Paragraph from '../components/paragraph'
+import { QuestionIcon } from '@chakra-ui/icons'
+import { Meta } from '../components/roadmap'
 
 const Roadmaps = () => (
-  <Layout title="Roadmaps">
-    <Container maxW = "container.lg">
-      <Heading as="h3" fontSize={20} mb={4}>
-        Roadmap Activations
-      </Heading>
-      <Heading as="h4" fontSize={14} mb={1}>
-        Click each level for more information.
-      </Heading>
+  
+<Layout title="Roadmaps">
+<Container maxW = "container.lg">
+  <Heading as="h3" fontSize={30} mb={4} >
+    Roadmap Activations
+  </Heading>
+  <Heading as="h4" fontSize={14} mb={1} paddingBottom={4}>
+    We are here for a good time and a long time. 
+  </Heading>
+  <Section delay={0.2} > 
+          <Heading as="h3" variant="section-title">
+          <QuestionIcon paddingRight={1.5} paddingTop={1} />
+          0%
+          </Heading>
+          <Meta>Startup</Meta>
+          <Paragraph >
+            Initial pre-sale for whitelisted members, then public mint initiated. 
+          </Paragraph>
+          </Section>      
+          <Section delay={0.3} > 
+          <Heading as="h3" variant="section-title" paddingTop={5}>
+          <QuestionIcon paddingRight={1.5} paddingTop={1}/>
+          25%
+          </Heading>
+          <Meta>Giveaways</Meta>
+          <Paragraph>
+            Holder prize giveaways and contests initiated. 
+          </Paragraph>
+          </Section>   
+          <Section delay={0.4} > 
+          <Heading as="h3" variant="section-title" paddingTop={5}>
+          <QuestionIcon paddingRight={1.5} paddingTop={1}/>
+          50%
+          </Heading>
+          <Meta>Community Fund</Meta>
+          <Paragraph>
+          100 ETH as well as 10% of secondary market sale royalites will be dedicated to the fund.
+           </Paragraph>
+        </Section>
+        <Section delay={0.5} > 
+          <Heading as="h3" variant="section-title" paddingTop={5}>
+          <QuestionIcon paddingRight={1.5} paddingTop={1}/>
+          75%
+          </Heading>
+          <Meta>Charity donation</Meta>
+          <Paragraph>
+         Special donation to the American Society for the Prevention of Cruelty to Animals. 
+         </Paragraph>
+        </Section>
+        <Section delay={0.5} > 
+          <Heading as="h3" variant="section-title" paddingTop={5}>
+          <QuestionIcon paddingRight={1.5} paddingTop={1}/>
+          The Soxialverse
+          </Heading>
+          <Meta>100%</Meta>
+          <Paragraph>
+         Continued growth and expansion of the soxial network within the metaverse. Members will have access to upcoming soxial projects and apps in development. 
+         </Paragraph>
+        </Section>
+</Container>
+</Layout>
 
-      <SimpleGrid columns={[1, 1, 3]} gap={3}>
-        <Section>
-          <WorkGridItem id="phase1" title="0%" thumbnail={greenpug}>
-            Community Building.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem id="phase2" title="10%" thumbnail={redpug}>
-            Double Down. Increased promotion and charity donations initiated. 
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem id="phase3" title="20%" thumbnail={redpug}>
-            Early Bird Special. Early adopters and community member giveaways initiated.
-          </WorkGridItem>
-        </Section>
-
-        <Section delay={0.1}>
-          <WorkGridItem id="phase4" title="40%" thumbnail={redpug}>
-            Scholarship Program.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem id="phase5" title="50%" thumbnail={redpug}>
-            Community Fund. 
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem id="phase6" title="75%" thumbnail={redpug}>
-            MERCH. 
-          </WorkGridItem>
-        </Section>
-
-        <Section delay={0.2}>
-          <WorkGridItem id="phase7" title="90%" thumbnail={redpug}>
-            SOXIAL CLUB HQ.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem id="phase8" title="100%" thumbnail={redpug}>
-            The Soxial Security Club App.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem id="" title="Phase Two" thumbnail={redpug}>
-            Stay Tuned...
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
-  </Layout>
 )
 
 export default Roadmaps
